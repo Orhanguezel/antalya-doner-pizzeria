8a4c2c9631f49f2b529f17c6d1dbbbc08700c6e7
orhan <orhan.guezel@dci-student.org>
2024-07-06 23:47:35 +0200

    Footer bileşeni eklendi ve yalnızca ana sayfada görüntülenmesi sağlandı

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -88,10 +88,15 @@ const App = () => {
             <Route path="/admin" element={<AdminPanel />} />
           </Routes>
         </main>
-        <Footer />
+        <FooterWrapper />
       </Router>
     </AuthProvider>
   );
+};
+
+const FooterWrapper = () => {
+  const location = useLocation();
+  return location.pathname === '/' ? <Footer /> : null;
 };
 
 export default App;
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Footer.css';
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2024 Antalya Döner & Pizzeria. Alle Rechte vorbehalten.</p>
+    <footer className="footer-section">
+      <div className="navbar-logo">
+        <img src="img/logo/2.png" alt="Logo" />
+      </div>
+      <h2 className="foot-title margin-top">ANTALYA Döner & Pizzeria</h2>
+      <p className="foot-p">Kapellenplatz 1, 52457 Aldenhoven</p>
+      <div className="social">
+        <a className="foot-terms-link" href="https://www.facebook.com/profile.php?id=100063527353789" target="_blank" rel="noopener noreferrer">
+          <ion-icon className="icon-foot" name="logo-facebook"></ion-icon>
+        </a>
+        <a className="foot-terms-link" href="https://www.instagram.com/antalya_doner_pizzeria/" target="_blank" rel="noopener noreferrer">
+          <ion-icon className="icon-foot" name="logo-instagram"></ion-icon>
+        </a>
+      </div>
+      <p className="foot-terms">
+        <a className="foot-terms-link" href="https://policies.google.com/privacy?hl=en-US" target="_blank" rel="noopener noreferrer">
+          Privacy & Terms
+        </a>
+      </p>
+      <p className="copyright">© Copyright - All Rights Reserved to ANTALYA Döner & Pizzeria</p>
+      <p><a>Designed by OG</a></p>
     </footer>
   );
-}
+};
 
 export default Footer;
